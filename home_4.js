bfeb95f5d3648ada1b6504c1fb2188f736c67758
Groofidze <groofi@list.ru>
2021-12-04 16:25:29 +0300

    Home_4.1

--- home_4.js
+++ home_4.js
@@ -1,6 +1,4 @@
 let words = `'This is a longer card with supporting text below as a natural lead-in to additional content.' 'This content is a little bit longer.' 'This is aren't'`
-let re = /'/g
+re = /(\b|)'(?!\b)|(?!\b)'(\b|)/gi
 words = words.replace(re, '"')
-re = /\b"\b/g
-words = words.replace(re, "'")
 console.log(words)
